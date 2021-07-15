ca68add006e860e9169c16f95dceab51f377c245
joelv9000 <joelvelazquezvargasmm@gmail.com>
2021-07-15 14:38:28 -0500

    Variables Tipos De Datos

---
+++ VariablesTiposDeDatos/src/main/java/Tipos.java
@@ -0,0 +1,25 @@
+
+public class Tipos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Mundo");
+		
+		int numero1 = 30;
+		int numero2 =15;
+		int multiplicacion = numero1*numero2;
+		String cadena1 ="texto 1";
+		String cadena2 ="texto 2";
+		char letra = 'y';
+		
+		String oracion = cadena1+cadena2;
+		boolean mayor18=true;
+		System.out.println(oracion);
+		
+	}
+
+}
